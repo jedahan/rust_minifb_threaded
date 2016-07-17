e6fcd94a3ab4e07e5ec25e9552221f41ed1801d2
Jonathan Dahan <hi@jonathan.is>
2016-07-17 12:32:12 -0400

    as minimal as I can make it

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,29 @@
 extern crate minifb;
 
 use std::thread;
+use std::time::Duration;
+use std::thread::sleep;
 
-use minifb::WindowOptions;
+use minifb::{Key, WindowOptions};
 
-struct GameBoy {
-    running: bool
-}
-
-struct Screen {
-    window: minifb::Window,
-    buffer: Vec<u32>
-}
-
-
-impl Screen {
-    pub fn new(width: usize, height: usize) -> Screen {
-        println!("Screen::new");
-        let window = match minifb::Window::new("debug", width, height,
-                                                   WindowOptions::default()) {
-            Ok(win) => win,
-            Err(err) => {
-                panic!("Unable to create window {}", err);
-            }
-        };
-
-        Screen {
-            window: window,
-            buffer: vec![0; width * height]
-        }
-    }
-
-    pub fn run(&mut self) {
-        println!("Screen::run");
-        for i in self.buffer.iter_mut() {
-            *i = 70; // write something more funny here!
-        }
-        self.window.update_with_buffer(&self.buffer);
-        self.window.set_title("Screen running");
-    }
-}
-
-impl GameBoy {
-    pub fn new(width: usize, height: usize) -> GameBoy {
-        println!("GameBoy::new");
-
-        let _ = thread::spawn(move || {
-            println!("GameBoy::new in thread");
-            let mut screen = Screen::new(width, height);
-            screen.run();
-        });
-
-        GameBoy {
-            running: false
-        }
-    }
-
-    pub fn run(&mut self) {
-        println!("GameBoy::run");
-        self.running = true;
-    }
-}
+const WIDTH: usize = 288;
+const HEIGHT: usize = 160;
 
 fn main() {
-    let mut gameboy = GameBoy::new(160, 288);
-    gameboy.run();
+    let mut buffer = vec![0; WIDTH * HEIGHT];
+
+    let _ = thread::spawn(move || {
+        let mut window = minifb::Window::new("debug", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+        window.set_title("Screen loop");
+        
+        while window.is_open() && !window.is_key_down(Key::Escape) {
+            for i in buffer.iter_mut() {
+                *i = 70; // write something more funny here!
+            }
+            window.update_with_buffer(&buffer);
+            sleep(Duration::from_millis(16));
+        }
+    });
+
+    println!("hi from the main thread");
 }
