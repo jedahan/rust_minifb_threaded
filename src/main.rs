1fb6d63600e6b606a342c45fd49f66e85ebfcf2d
Jonathan Dahan <hi@jonathan.is>
2016-07-17 12:38:22 -0400

    sleep in main thread

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,27 @@ const HEIGHT: usize = 160;
 
 fn main() {
     let mut buffer = vec![0; WIDTH * HEIGHT];
+    let mut frames: usize = 0;
 
-    let _ = thread::spawn(move || {
+    thread::spawn(move || {
         let mut window = minifb::Window::new("debug", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
-        window.set_title("Screen loop");
-        
+
         while window.is_open() && !window.is_key_down(Key::Escape) {
+
             for i in buffer.iter_mut() {
                 *i = 70; // write something more funny here!
             }
+
             window.update_with_buffer(&buffer);
             sleep(Duration::from_millis(16));
+            frames = frames + 1;
+            let framecount = format!("Hello from screen {} (frames/60)", frames/60);
+            window.set_title(framecount.as_str());
         }
     });
 
-    println!("hi from the main thread");
+    loop {
+        println!("hi from the main thread");
+        sleep(Duration::from_millis(16000));
+    }
 }
