8eff84a31af8a2967902bb858e687ffba9e9cc75
Jonathan Dahan <hi@jonathan.is>
2016-07-17 12:23:57 -0400

    Dont share memory, just try a window

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,64 @@
 extern crate minifb;
 
 use std::thread;
-use std::sync::{Arc, RwLock};
-use std::time::Duration;
-use std::thread::sleep;
 
-const MEMORY_SIZE: usize = 16;
+use minifb::WindowOptions;
 
-pub struct GameBoy {
-    cpu: Cpu
+struct GameBoy {
+    running: bool
 }
 
-pub struct Cpu {
-    running: bool,
-    memory: Arc<RwLock<[u8; MEMORY_SIZE]>>,
-}
-
-pub struct Screen {
+struct Screen {
     window: minifb::Window,
-    memory: Arc<RwLock<[u8; MEMORY_SIZE]>>,
+    buffer: Vec<u32>
 }
 
-impl Cpu {
-    pub fn new(memory: Arc<RwLock<[u8; MEMORY_SIZE]>>) -> Cpu {
-        println!("Cpu::new");
-        Cpu {
-            running: false,
-            memory: memory,
-        }
-    }
-
-    pub fn run(&mut self) {
-        println!("Cpu::run");
-        self.running = true;
-        self.memory.write().unwrap()[0] = 7;
-        println!("  [0x00] {:?}", self.memory.read().unwrap()[0]);
-    }
-}
 
 impl Screen {
-    pub fn new(width: usize, height: usize, memory: Arc<RwLock<[u8; MEMORY_SIZE]>>) -> Screen {
+    pub fn new(width: usize, height: usize) -> Screen {
         println!("Screen::new");
-        let window = minifb::Window::new("debug",
-                                         width,
-                                         height,
-                                         minifb::WindowOptions {
-                                             borderless: true,
-                                             scale: minifb::Scale::X4,
-                                             ..Default::default()
-                                         })
-            .unwrap();
+        let window = match minifb::Window::new("debug", width, height,
+                                                   WindowOptions::default()) {
+            Ok(win) => win,
+            Err(err) => {
+                panic!("Unable to create window {}", err);
+            }
+        };
 
         Screen {
             window: window,
-            memory: memory,
+            buffer: vec![0; width * height]
         }
     }
 
     pub fn run(&mut self) {
-        let first_byte = self.memory.read().unwrap()[0];
         println!("Screen::run");
-        println!("  [0x00] {:?}", first_byte);
-        let title = format!("We are running now! {:?}", first_byte);
-        self.window.set_title(title.as_str());
+        for i in self.buffer.iter_mut() {
+            *i = 70; // write something more funny here!
+        }
+        self.window.update_with_buffer(&self.buffer);
+        self.window.set_title("Screen running");
     }
 }
 
 impl GameBoy {
     pub fn new(width: usize, height: usize) -> GameBoy {
         println!("GameBoy::new");
-        let memory = Arc::new(RwLock::new([0; MEMORY_SIZE]));
-
-        let memory_ref = memory.clone();
-        println!("GameBoy::new memory_ref cloned");
 
         let _ = thread::spawn(move || {
             println!("GameBoy::new in thread");
-            let mut screen = Screen::new(width, height, memory_ref);
+            let mut screen = Screen::new(width, height);
             screen.run();
         });
 
-        sleep(Duration::from_millis(1000));
-
         GameBoy {
-            cpu: Cpu::new(memory)
+            running: false
         }
     }
 
     pub fn run(&mut self) {
-        println!("Gameboy::run");
-        self.cpu.run();
+        println!("GameBoy::run");
+        self.running = true;
     }
 }
 
